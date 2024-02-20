09ee892642e4989d853af279ed4fbf82e794e2bd
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-20 09:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let student={
+	name: "raghav",
+}
+let fn = getKeys(){
+	return(Object.keys(this));
+}
+Object.prototype.getKeys = fn;
+console.log(student.getKeys());
